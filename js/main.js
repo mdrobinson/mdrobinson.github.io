5fd24fbb1b71a30558217a3b6d0c599789a1b01b
Mark Robinson <mark@codecornerapps.com>
2015-09-30 23:59:14 -0400

    Google Analytics and Disquis enabled

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ $("#search-query").on('blur', function () {
 });
 
 $("#about").click(function() {
-  if (window.location.href.indexOf('-') != -1 || window.location.href.indexOf('?q')) {
+  if (window.location.href.indexOf('-') != -1 || window.location.href.indexOf('?q') != -1) {
     window.location.href = "/?about"
   }else if (window.location.href.indexOf('?') != -1 && window.location.href.indexOf('?about') == -1) {
     window.location.href = "/"
